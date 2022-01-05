5d396752abc8b0355eee237a639a7036f092263e
Colleen Lohr <cmlohr@outlook.com>
2022-01-04 18:59:01 -0600

    added dialogue arrays

---
+++ hello.py
@@ -0,0 +1,11 @@
+# file for dialogue arrays.
+
+greetings = ["greetings", "hi", "howdy", "welcome", "bonjour", "buenas noches", "buenos dias", "good day", "good morning", "hey", "hi-ya", "how are you", "how goes it?", "howdy-do", "shalom", "what's happening?", "yo"]
+
+# there's a bug in the function that calls this array
+# hello_check = ['$hello Testy', '$hello testy', '$heya', '$hi', '$yo', "$what's up?", "$hello", "$on?", "$testy", "$are you there?"]
+
+predictions_8ball = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.", "You may rely on it.", 
+"As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+
