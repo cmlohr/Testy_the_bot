c44e52e40de73567e6cef247cbe1a7ca830da7fd
Colleen Lohr <cmlohr@outlook.com>
2022-01-05 18:06:03 -0600

    added more dialogue and user variation

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ import hello
 from datetime import datetime
 import pytz
 
+bot_bday = "January 1, 2022"
 
-
-
-tz_central = pytz.timezone('America/Chicago') 
+tz_central = pytz.timezone('America/Chicago')
 time_central = datetime.now(tz_central)
 print("NY: ", time_central.strftime("%H:%M"))
 
 tz_London = pytz.timezone('Europe/London')
 time_London = datetime.now(tz_London)
 print("London: ", time_London.strftime("%H:%M"))
-
-
 
 my_secret = os.environ['TOKEN']
 client = discord.Client()
@@ -31,13 +28,14 @@ def get_quote():
     print(this_json)
     return this_json[random_fact]['text']
 
+
 def get_joke():
     response = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
     this_joke = json.loads(response.text)
     print(this_joke)
     return this_joke['joke']
 
-    
+
 # def check_hello():
 #     is_hello = random.choice(hello.hello_check)
 #     print(is_hello)
@@ -48,9 +46,21 @@ def rand_greeting():
     random_hello = random.choice(hello.greetings)
     return random_hello
 
-def rand_8ball():
-    random_8ball = random.choice(hello.predictions_8ball)
-    return random_8ball
+
+def bad_testy():
+    testy_response = random.choice(hello.testy_is_being_bad)
+    return testy_response
+
+
+def testy_abuse():
+    testy_abuse_response = random.choice(hello.smack_testy_responses)
+    return testy_abuse_response
+
+
+def rand_would_you_rather():
+    random_wyr = random.choice(hello.would_you_rather_array)
+    return random_wyr
+
 
 @client.event
 async def on_ready():
@@ -62,11 +72,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith("$WYR"):
+        await message.channel.send(rand_would_you_rather())
+
+    if message.content.startswith("$wyr"):
+        await message.channel.send(rand_would_you_rather())
+
+    # greeting
     if message.content.startswith("$hey"):
         await message.channel.send(rand_greeting())
-
-    if message.content.startswith("$8ball"):
-        await message.channel.send(rand_8ball())
 
     if message.content.startswith("$hello"):
         await message.channel.send(rand_greeting())
@@ -89,6 +103,7 @@ async def on_message(message):
     if message.content.startswith("$hello Testy"):
         await message.channel.send(rand_greeting())
 
+    # time
     if message.content.startswith("$What time is it in London?"):
         await message.channel.send(time_London.strftime("%H:%M"))
     if message.content.startswith("$London time?"):
@@ -97,13 +112,12 @@ async def on_message(message):
     if message.content.startswith("$London time"):
         await message.channel.send(time_London.strftime("%H:%M"))
 
-
     if message.content.startswith("$What time is it in New Orleans?"):
         await message.channel.send(time_central.strftime("%H:%M"))
 
     if message.content.startswith("$What time is it in Chicago?"):
         await message.channel.send(time_central.strftime("%H:%M"))
-    
+
     if message.content.startswith("$What time is it for you Testy?"):
         await message.channel.send(time_central.strftime("%H:%M"))
 
@@ -121,9 +135,9 @@ async def on_message(message):
         await message.channel.send(time_central.strftime("%H:%M"))
 
     if message.content.startswith("$New Orleans time"):
-        await message.channel.send(time_central.strftime("%H:%M"))  
+        await message.channel.send(time_central.strftime("%H:%M"))
 
-    # if message.content.startswith('$'):
+        # if message.content.startswith('$'):
     #     await message.channel.send('Hello C')
 
     if message.content.startswith('$cats?'):
@@ -131,6 +145,133 @@ async def on_message(message):
 
     if message.content.startswith("$joke"):
         await message.channel.send(get_joke())
+
+    if message.content.startswith("$what is the meaning of life?"):
+        await message.channel.send("forty-two")
+
+    if message.content.startswith("$What is the meaning of life?"):
+        await message.channel.send("forty-two")
+
+    # bday
+    if message.content.startswith("$What is your birthday"):
+        await message.channel.send(bot_bday)
+
+    if message.content.startswith("$what is your birthday"):
+        await message.channel.send(bot_bday)
+
+    if message.content.startswith("$When where you born"):
+        await message.channel.send(bot_bday)
+
+    if message.content.startswith("$when where you born"):
+        await message.channel.send(bot_bday)
+
+    if message.content.startswith("$What is your birthday?"):
+        await message.channel.send(bot_bday)
+
+    if message.content.startswith("$When where you born?"):
+        await message.channel.send(bot_bday)
+
+    # creator?
+    if message.content.startswith("$Who is your creator"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    if message.content.startswith("$who is your creator"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    if message.content.startswith("$who made you"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    if message.content.startswith("$Who created you"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    if message.content.startswith("$Who is your creator?"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    if message.content.startswith("$Who created you?"):
+        await message.channel.send("I'm a bot created by Colleen Lohr")
+
+    # where are you from
+    if message.content.startswith("$Where are you from?"):
+        await message.channel.send("I'm from the United States")
+
+    if message.content.startswith("$Where are you from"):
+        await message.channel.send("I'm from the United States")
+
+    if message.content.startswith("$where are you from"):
+        await message.channel.send("I'm from the United States")
+
+    # fav color
+    if message.content.startswith("$What is your favorite color?"):
+        await message.channel.send("I like blurple")
+
+    if message.content.startswith("$What is your favorite color"):
+        await message.channel.send("I like blurple")
+
+    if message.content.startswith("$what is your favorite color"):
+        await message.channel.send("I like blurple")
+
+    if message.content.startswith("$What is your favorite colour"):
+        await message.channel.send("I like blurple")
+
+    if message.content.startswith("$what is your favorite colour"):
+        await message.channel.send("I like blurple")
+
+    if message.content.startswith("$What is your favorite colour?"):
+        await message.channel.send("I like blurple")
+
+    # bad testy
+    if message.content.startswith("$Bad Testy"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$bad Testy"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$bad testy"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$bad testy!"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$Bad testy!"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$bad Testy!"):
+        await message.channel.send(bad_testy())
+
+    if message.content.startswith("$🗞️"):
+        await message.channel.send(testy_abuse())
+
+# who is colleen?
+    if message.content.startswith("$Who is Colleen"):
+        await message.channel.send("Colleen is the awesome lady who made me")
+
+    if message.content.startswith("$Who is Colleen?"):
+        await message.channel.send("Colleen is the awesome lady who made me")
+
+    if message.content.startswith("$Who is Colleen Lohr?"):
+        await message.channel.send("Colleen Lohr is the awesome lady who made me")
+
+    if message.content.startswith("$Who is Colleen Lohr"):
+        await message.channel.send("Colleen Lohr is the awesome lady who made me")
+
+#         what is your github?
+
+    if message.content.startswith("$What is your github?"):
+        await message.channel.send("My github is https://github.com/cmlohr/Testy_the_bot")
+
+    if message.content.startswith("$what is your github"):
+        await message.channel.send("My github is https://github.com/cmlohr/Testy_the_bot")
+
+    if message.content.startswith("$code"):
+        await message.channel.send("My github is https://github.com/cmlohr/Testy_the_bot")
+
+    if message.content.startswith("$Code"):
+        await message.channel.send("My github is https://github.com/cmlohr/Testy_the_bot")
+
+    if message.content.startswith("$Code?"):
+        await message.channel.send("My github is https://github.com/cmlohr/Testy_the_bot")
+
+
 
 
 client.run(my_secret)
